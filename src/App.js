ab5c626ccef4437d80b1df24d69835c7b4eded57
Aditya <abhogte@gmail.com>
2021-03-18 01:29:57 -0500

    removetestimonials

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-        <Testimonials data={this.state.resumeData.testimonials}/>
+        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
         <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
